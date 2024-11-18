f66d4c200d93b0ad3f2b41cb88b75de63efab5fa
mawdx <chen_wen@hotmail.com>
2024-11-18 22:47:46 +0800

    init

--- src/cissy/task.clj
+++ src/cissy/task.clj
@@ -5,13 +5,12 @@
 (defrecord TaskNodeInfo [^String node-id ^String node-name])
 
 ;定义一个任务执行依赖
-(deftype TaskNodeGraph [child-node-map parent-node-map all-node-id-set task-node-tree] 
+(deftype TaskNodeGraph [child-node-map parent-node-map all-node-id-set task-node-tree]
 
-  ;获取启动节点
+  ; 获取启动节点
   Object
   (get-startup-nodes [this]
-    (prn "hello"))
-  )
+    (prn "hello")))
 
 ;定义一个任务类型
 (defrecord TaskInfo [^String task-id ^String task-name
