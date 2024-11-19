84e7324eea318ebb70157c59b24a1103789de1d1
mawdx <chen_wen@hotmail.com>
2024-11-19 21:33:37 +0800

    init

--- src/cissy/task.clj
+++ src/cissy/task.clj
@@ -1,17 +1,33 @@
 (ns cissy.task #_{:clj-kondo/ignore [:syntax]}
-               (:import [java.lang.String]))
+    (:import [java.lang.String]))
+
+(defprotocol TaskNodeGraphDef
+  ;获取启动节点
+  (get-startup-nodes [])
+  ;构造graph
+  (build-node-tree [])
+  ;添加节点对
+  (add-node-pair [from-node to-node])
+  ;获取子节点列表
+  (get-child-nodes [node-id])
+  ;获取父节点列表
+  (get-parent-nodes [node-id]))
 
 ;定义一个节点类型
 (defrecord TaskNodeInfo [^String node-id ^String node-name])
 
 ;定义一个任务执行依赖
 (deftype TaskNodeGraph [child-node-map parent-node-map all-node-id-set task-node-tree]
-  ; 获取启动节点
-  Object
+  TaskNodeGraphDef
+  
   (get-startup-nodes [this]
-    (prn "hello")))
+    )
+  (build-node-tree [this])
+  (add-node-pair [this from-node to-node])
+  (get-child-nodes [this])
+  (get-parent-nodes [this])
+  )
 
 ;定义一个任务类型
 (defrecord TaskInfo [^String task-id ^String task-name
-                     ^String task-exec-type sched-info ^TaskNodeGraph node-graph task-config]
-  )
+                     ^String task-exec-type sched-info ^TaskNodeGraph node-graph task-config])
