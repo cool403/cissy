10a932dc91753e6f6526cffd21ce2caff7e14eef
cxiaoer <chen_wen@hotmail.com>
2024-11-20 13:13:05 +0800

    init

--- src/cissy/task.clj
+++ src/cissy/task.clj
@@ -16,6 +16,17 @@
 ;定义一个节点类型
 (defrecord TaskNodeInfo [^String node-id ^String node-name])
 
+
+(defn _add-node-pair [from-node to-node d]
+  (when (not (nil? from-node))
+    (let [from-node-id (:node-id from-node)]
+      (cond
+        (nil? to-node) ()
+        (contains? (get d from-node-id) (:node-id to-node)) (.add (get d from-node-id) from-node)
+        :else (-> (java.util.ArrayList.)
+                  (.add to-node)
+                  ((fn [x] (.put d from-node-id x))))))))
+
 ;定义一个任务执行依赖
 (defrecord TaskNodeGraph [child-node-map parent-node-map all-node-id-set task-node-tree]
   TaskNodeGraphDef
@@ -28,9 +39,16 @@
             (= 0 (count parent-nodes)) (.add res it))))
       res))
   (build-node-tree [this])
-  (add-node-pair [this from-node to-node])
-  (get-child-nodes [this])
-  (get-parent-nodes [this]))
+  (add-node-pair [this from-node to-node]
+    (_add-node-pair from-node to-node child-node-map)
+    (_add-node-pair to-node from-node parent-node-map)
+    (cond
+      (not (nil? from-node)) (.add all-node-id-set from-node)
+      (not (nil? to-node)) (.add all-node-id-set to-node)))
+  (get-child-nodes [this node-id]
+    (get child-node-map node-id))
+  (get-parent-nodes [this node-id]
+    (get parent-node-map node-id)))
 
 ;定义一个任务类型
 (defrecord TaskInfo [^String task-id ^String task-name
