9f941b4f9133a8313c9512837d2f2470ecc27b09
cxiaoer <chen_wen@hotmail.com>
2024-11-22 18:43:02 +0800

    init

--- src/cissy/loader.clj
+++ src/cissy/loader.clj
@@ -1,1 +1,15 @@
-(ns cissy.loader)
+(ns cissy.loader
+;;   (:import (cissy.task TaskInfo))
+  (:require [cheshire.core :as json]))
+
+(def demo-json "{\n    \"task_name\":\"demo\",\n    \"nodes\":\"demo->\",\n    \"demo\":{\n        \n    }\n}")
+;从 json 中解析任务
+(defn get-task-from-json [^String task-json]
+  "从 json 中解析任务。
+  ;ret TaskInfo 返回一个任务类型"
+  ;json->keyword map,keyword map才能用(:name 方式访问)
+  (def task-map (json/parse-string task-json #(keyword %)))
+  (let [{task-name :task_name nodes :nodes} task-map]
+    )
+  (json/parse-string demo-json #(keyword %))
+  )
