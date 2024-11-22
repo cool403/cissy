13823a2d146c817d6a75ec309e66935193896eb6
mawdx <chen_wen@hotmail.com>
2024-11-22 23:33:02 +0800

    init

--- src/cissy/sched.clj
+++ src/cissy/sched.clj
@@ -1,12 +1,17 @@
 (ns cissy.sched
-  (:import (cissy.executions TaskExecutionInfo TaskSched)))
+  (:import (cissy.executions TaskExecutionInfo TaskSched))
+  (:require [taoensso.timbre :as timbre]))
 
 
 
 (deftype ExecutionOnceSched []
   TaskSched
-  (get-task-sched-type [] "exec_once")
-  (get-task-sched-name [] "执行一次")
-  (sched-task-execution [^TaskExecutionInfo task-execution-info]
-    ())
+  (get-task-sched-type [this] "exec_once")
+  (get-task-sched-name [this] "执行一次")
+  (sched-task-execution [this ^TaskExecutionInfo task-execution-info]
+    (timbre/info "execute task info local mod")
+    ((let [{task-info  :task-info
+            start-time :start-time} @task-execution-info]
+       (timbre/info "start to get startup nodes for " (:task-exec-type task-info))
+       )))
   )
