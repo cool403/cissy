dbcc082b29791ae02af56e27b443b243d34cd387
mawdx <chen_wen@hotmail.com>
2024-11-23 15:54:32 +0800

    init

--- src/cissy/core.clj
+++ src/cissy/core.clj
@@ -1,7 +1,30 @@
 (ns cissy.core
-  (:require [cissy.executions :refer [TaskExecutionInfo]]))
+  (:require
+   [cissy.executions :refer [TaskExecutionInfo]]
+   [cissy.task :as task]
+   [taoensso.timbre :as timbre]))
 
+(comment
+  (defprotocol Human
+    (age []))
+  (deftype Jissy [name age]
+    Human
+    (age [this] age))
+  (def j (->Jissy "张三" 23))
+  (age j))
+
+
+;A----->B---------->F
+;|                  |
+; ----->C---->D------
+;|            |
+; ----->E------
 (defn run-task-in-local
   "docstring"
   [^TaskExecutionInfo task-execution-info]
-  (let [{} @task-execution-info]))
+  (timbre/info "start to get startup nodes for task")
+  (let [{^task/TaskInfo task-info  :task-info} @task-execution-info
+        {^task/TaskNodeGraph node-graph :node-graph} task-info
+        startup-nodes (get-startup-nodes node-graph)]
+    (if (<= (count startup-nodes) 0) (timbre/warn "未匹配到启动节点")
+        ())))
