0302ba874a546c0b2e461da1a1c4f1e2ae9f5a0c
mawdx <chen_wen@hotmail.com>
2024-11-23 17:41:34 +0800

    init

--- src/cissy/registry.clj
+++ src/cissy/registry.clj
@@ -1,4 +1,12 @@
 (ns cissy.registry)
 
+;注册器
+(def task-node-register (atom {}) )
 
+
+(comment
+  (defn a [x] (inc x))
+  (reset! task-node-register (assoc @task-node-register :12 a))
+  ((:12 @task-node-register) 12)
+  )
 
