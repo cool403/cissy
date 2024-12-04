243c668c5dfe24b6a0a23a217a95db237b08861a
mawdx <chen_wen@hotmail.com>
2024-12-04 21:40:54 +0800

    init

--- src/cissy/commands.clj
+++ src/cissy/commands.clj
@@ -1,1 +1,16 @@
-(ns cissy.commands)
+(ns cissy.commands 
+  (:require
+   [taoensso.timbre :as timbre]
+   [cissy.loader :as loader]))
+
+(defn demo
+  "任务配置json样例"
+  [options]
+  )
+
+(defn start
+  "通过配置文件启动任务"
+  [options]
+  (timbre/info "执行任务启动命令" options)
+  (-> ())
+  )
