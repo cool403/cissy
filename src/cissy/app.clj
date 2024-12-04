6f9d0feeeb89321c177848d2405e783591600f10
mawdx <chen_wen@hotmail.com>
2024-12-04 22:34:36 +0800

    init

--- src/cissy/app.clj
+++ src/cissy/app.clj
@@ -1,40 +1,14 @@
 (ns cissy.app
-  (:require [cissy.task :refer [TaskNodeInfo]]
-            [clojure.tools.cli :as cli]
-            [clojure.string :as str])
-  (:import (java.net InetAddress)))
+  (:require [clojure.tools.cli :as cli]
+            [clojure.string :as str]
+            [cissy.commands :as commands]))
 
 (def cli-options
-  [;; First three strings describe a short-option, long-option with optional
-   ;; example argument description, and a description. All three are optional
-   ;; and positional.
-   ["-p" "--port PORT" "Port number"
-    :default 80
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ["-H" "--hostname HOST" "Remote host"
-    :default (InetAddress/getByName "localhost")
-    ;; Specify a string to output in the default column in the options summary
-    ;; if the default value's string representation is very ugly
-    :default-desc "localhost"
-    :parse-fn #(InetAddress/getByName %)]
-   ;; If no required argument description is given, the option is assumed to
-   ;; be a boolean option defaulting to nil
-   [nil "--detach" "Detach from controlling process"]
-   ["-v" nil "Verbosity level; may be specified multiple times to increase value"
-    ;; If no long-option is specified, an option :id must be given
-    :id :verbosity
-    :default 0
-    ;; Use :update-fn to create non-idempotent options (:default is applied first)
-    :update-fn inc]
-   ["-f" "--file NAME" "File names to read"
-    :multi true ; use :update-fn to combine multiple instance of -f/--file
-    :default []
-    ;; with :multi true, the :update-fn is passed both the existing parsed
-    ;; value(s) and the new parsed value from each option
-    :update-fn conj]
-   ;; A boolean option that can explicitly be set to false
-   ["-d" "--[no-]daemon" "Daemonize the process" :default true]
+  [["-c" "--config config" "任务配置描述,需要是json格式"
+    ;; :default 80
+    ;; :parse-fn #(Integer/parseInt %)
+    ;; :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+    ]
    ["-h" "--help"]])
 
 
@@ -47,21 +21,18 @@
         options-summary
         ""
         "Actions:"
-        "  start    Start a new server"
-        "  stop     Stop an existing server"
-        "  status   Print a server's status"
+        "  start  启动任务"
+        "  demo   获取任务json dmeo"
         ""
-        "Please refer to the manual page for more information."]
+        "-h help 获取更多支持"
+        ]
        (str/join \newline)))
 
 (defn error-msg [errors]
   (str "The following errors occurred while parsing your command:\n\n"
        (str/join \newline errors)))
 
-(defn validate-args
-  "Validate command line arguments. Either return a map indicating the program
-  should exit (with an error message, and optional ok status), or a map
-  indicating the action the program should take and the options provided."
+(defn validate-args 
   [args]
   (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)]
     (cond
@@ -71,7 +42,7 @@
       {:exit-message (error-msg errors)}
       ;; custom validation on arguments
       (and (= 1 (count arguments))
-           (#{"start" "stop" "status"} (first arguments)))
+           (#{"start" "demo"} (first arguments)))
       {:action (first arguments) :options options}
       :else ; failed custom validation => exit with usage summary
       {:exit-message (usage summary)})))
@@ -85,6 +56,5 @@
     (if exit-message
       (exit (if ok? 0 1) exit-message)
       (case action
-        "start"  (prn options)
-        "stop"   (prn options)
-        "status" (prn options)))))
+        "start"  (commands/start options)
+        "demo" (commands/demo)))))
