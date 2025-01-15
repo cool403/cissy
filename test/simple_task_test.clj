a670efccbe3048efcf54c86e41d20266cd0051ea
mawdx <chen_wen@hotmail.com>
2025-01-15 12:47:55 +0800

    修改单元测试

--- test/simple_task_test.clj
+++ test/simple_task_test.clj
@@ -2,27 +2,42 @@
   (:require
    [cissy.executions :as executions]
    [cissy.loader :as loader]
+   [clojure.string :as str]
    [clojure.test :as test]
-   [ding :as ding]
-   [dong :as dong]))
+   [ding]
+   [dong]))
 
 (def simple-task-json
-  (str
-   "{\"task_name\":\"hello windows\",
-\"nodes\":\"ding->dong\",
-\"datasources\":{
-},
-\"ding\":{
-    \"threads\":3
-},
-\"dong\":{
-    \"threads\":2
-}}"))
+  (str (->> ["{"
+             "    \"datasource\": {"
+             "    },"
+             "	\"task_group\":[{"
+             "		\"task_group_name\":\"hello windows\","
+             "		\"nodes\": \"ding->dong;\","
+             "		\"ding\": {"
+             "			\"threads\":2"
+             "		},"
+             "		\"dong\": {"
+             "			\"threads\":3"
+             "		},"
+             "		\"tasks\":["
+             "			{"
+             "				\"ding\": {"
+             "					},"
+             "				\"dong\": {"
+             "				}"
+             "			}"
+             "		]}"
+             "	]"
+             "  }  "
+             ""]
+            (str/join \newline))))
 
 (test/deftest simple-task-test
   (test/testing "最简单的ding dong任务测试"
     (prn simple-task-json)
-    (let [task-info (loader/get-task-from-json simple-task-json)
+    (let [task-info-lst (loader/get-task-from-json simple-task-json)
+          task-info (first task-info-lst)
           sched-info              (:sched-info @task-info)
           new-task-execution-info (executions/new-task-execution-info)]
       (reset! new-task-execution-info (assoc @new-task-execution-info :task-info task-info))
