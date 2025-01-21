fff7a14124afed298497accdf18ea1eae13443ab
chen_ <1212@hotmail.com>
2025-01-21 22:35:22 +0800

    用edn格式重新定义任务配置

---
+++ test/task_config_edn_test.clj
@@ -0,0 +1,14 @@
+(ns task-config-edn-test
+  (:require [clojure.test :as test]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+
+(def config-path "task_config.edn" )
+
+(test/deftest task-config-edn-test
+  (test/testing "测试edn格式配置加载"
+    (let [cfg (edn/read-string (slurp (io/resource config-path)))]
+      (println cfg)
+      (println (prn-str cfg))
+      (test/is (not (nil? cfg))))))
