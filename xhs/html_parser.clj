a9dcd358dc436be3619cd85159411a778f4e5126
mawdx <chen_wen@hotmail.com>
2025-03-01 07:36:46 +0800

    fix

--- xhs/html_parser.clj
+++ xhs/html_parser.clj
@@ -1,10 +1,19 @@
 (ns xhs.html-parser
-  (:require [taoensso.timbre :as timbre]))
+  (:require [taoensso.timbre :as timbre])
+  (:import [com.vladsch.flexmark.html2md.converter FlexmarkHtmlConverter]))
 
 ;; Define a multi-method for parsing HTML content
 (defmulti parse-html-content (fn [page-content] (:source page-content)))
 
+;; FlexmarkHtmlConverter singleton
+(defonce html-to-markdown-converter (-> (FlexmarkHtmlConverter/builder nil) (.build)))
+
+;; html to markdown
+(defn html-to-markdown [html]
+  (timbre/info "html-to-markdown")
+  (time (str (.convert html-to-markdown-converter html))))
+
 ;; parse xhs-index
 (defmethod parse-html-content :xhs-index
-    [{:keys [page content]}]
+  [{:keys [page content]}]
   (timbre/info "parse-html-content :xhs-index"))
